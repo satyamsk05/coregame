1782323b976b3032a4f0013c38db0f8366c08ed2
satyamsk05 <your-email@example.com>
2026-08-14 11:52:25 +0530

    Update SoundManager to call playTick from sound_helper to enable synthesized Web Audio tick sounds in browser

--- lib/utils/sound_manager.dart
+++ lib/utils/sound_manager.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/services.dart';
+import 'package:flutter/foundation.dart';
+import 'sound_helper.dart' as helper;
 
 class SoundManager {
   static bool soundOn = true;
 
   static void playClick() {
     if (soundOn) {
-      SystemSound.play(SystemSoundType.click);
+      if (kIsWeb) {
+        helper.playTick();
+      } else {
+        helper.playTick();
+        SystemSound.play(SystemSoundType.click);
+      }
     }
   }
 }
